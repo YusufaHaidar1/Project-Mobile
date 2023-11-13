01c8f92775ebf08a5ccbb5356237e53fc5439a2c
Yusufa Haidar <91399445+YusufaHaidar1@users.noreply.github.com>
2023-11-13 21:49:28 +0700

    Update Main

--- uts/lib/main.dart
+++ uts/lib/main.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:uts/screens/Home.dart';
-import 'package:uts/screens/Detail.dart';
-import 'package:uts/screens/detail_complain.dart';
 
 void main() {
-  runApp(MaterialApp(
-    initialRoute: '/',
-    routes: {
-      '/' : (_) => const Home(),
-      "/detail": (_) => Detail(),
-      "/detail-complain": (_) => const DetailComplain(),
-    },
-  ));
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+      ),
+      home: Home(),
+    );
+  }
 }
 
